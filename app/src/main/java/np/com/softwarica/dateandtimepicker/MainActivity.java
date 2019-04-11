da1a209a3659b3df1edd756b1dd57ab8ad8be26f
Softwarica College Lab <softwarica@softwarica.com>
2019-04-11 10:49:15 +0545

    Date completed

--- app/src/main/java/np/com/softwarica/dateandtimepicker/MainActivity.java
+++ app/src/main/java/np/com/softwarica/dateandtimepicker/MainActivity.java
@@ -1,13 +1,50 @@
 package np.com.softwarica.dateandtimepicker;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.Year;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private EditText et1;
+    private EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+
+        et1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDatePicker();
+            }
+        });
+    }
+    private void loadDatePicker() {
+        final Calendar c= Calendar.getInstance();
+        int year= c.get(Calendar.YEAR);
+        int month= c.get(Calendar.MONTH);
+        int day= c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,this,year,month,day);
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        String date=  month + "/" + dayOfMonth + "/" + year;
+        et1.setText(date);
+
     }
 }
